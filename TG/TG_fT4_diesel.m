90c14e1aaa7b9bff568e61ca3aadd3b7dfcb0f96
Dave-999 <david.dispas@gmx.fr>
2016-12-11 21:05:26 +0100

    Déplacement des fonctions dans dossier TG

--- TG/TG_fT4_diesel.m
+++ TG/TG_fT4_diesel.m
@@ -1,6 +1,10 @@
 function f = TG_fT4_diesel(p3,p4,T3,T4,eta_piT,x_O2_massic,R_g,lambda,m_a1)
+if T4>300
 cp_mean=integral(@(t) (janaf('c','CO2',t)*44/(12^2+23)*48/4+janaf('c','H2O',t)*18/(12^2+23)*46/4+lambda*m_a1*(1-x_O2_massic)*janaf('c','N2',t)+(lambda*m_a1*x_O2_massic-m_a1*x_O2_massic)*janaf('c','O2',t))/(1+lambda*m_a1),T2,T3)/(T4-T3);
+else
+cp_mean=(janaf('c','CO2',300)*44/(12^2+23)*48/4+janaf('c','H2O',300)*18/(12^2+23)*46/4+lambda*m_a1*(1-x_O2_massic)*janaf('c','N2',300)+(lambda*m_a1*x_O2_massic-m_a1*x_O2_massic)*janaf('c','O2',300))/(1+lambda*m_a1);
 f=(p4/p3)^(eta_piT*(R_g/1000)/cp_mean)-T4/T3;
+
 end
 
 
--- TG/TG_fT4_methane.m
+++ TG/TG_fT4_methane.m
@@ -1,5 +1,8 @@
 function f = TG_fT4_methane(p3,p4,T3,T4,eta_piT,x_O2_massic,R_g,lambda,m_a1)
+if T4>300
 cp_mean=integral(@(t) (janaf('c','CO2',t)/16*44+janaf('c','H2O',t)/8*18+lambda*m_a1*(1-x_O2_massic)*janaf('c','N2',t)+(lambda*m_a1*x_O2_massic-m_a1*x_O2_massic)*janaf('c','O2',t))/(1+lambda*m_a1),T3,T4)/(T4-T3);
-f=(p4/p3)^(eta_piT*(R_g/1000)/cp_mean)-T4/T3;
+else
+    cp_mean=(janaf('c','CO2',300)/16*44+janaf('c','H2O',300)/8*18+lambda*m_a1*(1-x_O2_massic)*janaf('c','N2',300)+(lambda*m_a1*x_O2_massic-m_a1*x_O2_massic)*janaf('c','O2',300))/(1+lambda*m_a1);
 end
-
+    f=(p4/p3)^(eta_piT*(R_g/1000)/cp_mean)-T4/T3;
+end
